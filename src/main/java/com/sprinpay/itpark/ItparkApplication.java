540bbe40f28d8200eb07d904a77fe22ca94e091c
Debugger09 <kuitoange@email.com>
2024-03-16 11:27:33 +0100

    mon projet springboot initialise sur spring initializ

---
+++ src/main/java/com/sprinpay/itpark/ItparkApplication.java
@@ -0,0 +1,13 @@
+package com.sprinpay.itpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprinpay/itpark/ItparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinpay.itpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
